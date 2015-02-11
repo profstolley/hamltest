111a45c91aa071df024198107ec08247bce66539
Karl Stolley <karl.stolley@gmail.com>
2015-02-10 20:58:49 -0600

    Ran the migration to add age to Person

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211005958) do
+ActiveRecord::Schema.define(version: 20150211020449) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "favorite_color"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "age"
   end
 
 end
